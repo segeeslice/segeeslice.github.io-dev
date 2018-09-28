bb01cf47274b2a307060b05ec18a369a16652e7f
Dustin Seger <dustin.seger@hotmail.com>
2018-09-28 09:09:40 -0400

    Background image - probabily temp

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <v-layout row wrap>
       <v-flex xs12>
-        <v-card height="100vh" class="text-xs-center">
-            <div style="padding-top:40vh;">
+        <v-card color="transparent" height="100vh" class="text-xs-center">
+            <div style="padding-top:40vh; color:white;">
               <h1>Dustin Seger</h1>
               <h3>A portfolio</h3>
             </div>
@@ -11,8 +11,8 @@
       </v-flex>
 
       <v-flex xs12>
-        <v-card>
-          <v-card-text>
+        <v-card color="transparent">
+          <v-card-text style="color:white">
             <h2>Quick Summary</h2>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hendrerit gravida rutrum quisque non tellus orci ac auctor. Scelerisque felis imperdiet proin fermentum leo vel orci. Turpis egestas sed tempus urna et pharetra. Id volutpat lacus laoreet non curabitur gravida arcu. Amet purus gravida quis blandit turpis cursus. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Ut tellus elementum sagittis vitae et leo duis ut. Faucibus nisl tincidunt eget nullam non. Tempor commodo ullamcorper a lacus. Vestibulum lorem sed risus ultricies tristique. Erat nam at lectus urna duis. Facilisi cras fermentum odio eu. Et malesuada fames ac turpis egestas sed tempus urna. In hac habitasse platea dictumst vestibulum rhoncus est. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Nullam non nisi est sit amet facilisis magna etiam tempor. Augue eget arcu dictum varius duis at consectetur lorem.
@@ -28,8 +28,8 @@
       </v-flex>
 
       <v-flex xs12>
-        <v-card>
-          <v-card-text>
+        <v-card color="transparent">
+          <v-card-text style="color:white">
             <h2>About me</h2>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hendrerit gravida rutrum quisque non tellus orci ac auctor. Scelerisque felis imperdiet proin fermentum leo vel orci. Turpis egestas sed tempus urna et pharetra. Id volutpat lacus laoreet non curabitur gravida arcu. Amet purus gravida quis blandit turpis cursus. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Ut tellus elementum sagittis vitae et leo duis ut. Faucibus nisl tincidunt eget nullam non. Tempor commodo ullamcorper a lacus. Vestibulum lorem sed risus ultricies tristique. Erat nam at lectus urna duis. Facilisi cras fermentum odio eu. Et malesuada fames ac turpis egestas sed tempus urna. In hac habitasse platea dictumst vestibulum rhoncus est. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Nullam non nisi est sit amet facilisis magna etiam tempor. Augue eget arcu dictum varius duis at consectetur lorem.
@@ -65,6 +65,10 @@ export default {
 <style>
 #app {
   font-family: 'Roboto';
+  background-image: url('./assets/mountain-sunset-min.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
 }
 h1 {
   font-size: 60px;
